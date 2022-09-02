b39e3d0b042db1364318d20854a90dfc4494f330
CeceliaShuai-xy <xinyushuaic@gmail.com>
2022-09-01 21:42:31 -0400

    Use the provided sleep time to calculate wakeup times.

--- script.js
+++ script.js
@@ -14,9 +14,25 @@ function handleOnClick() {
   let hours = document.getElementById("hours");
   hours.innerHTML = "";
 
+  var opt1 = document.getElementById("hourtime");
+  var opt2 = document.getElementById("minutetime");
+  var opt3 = document.getElementById("daytime");
+
+  let hour = opt1.options[opt1.selectedIndex].value;
+  let minute = opt2.options[opt2.selectedIndex].value;
+  let daytime = opt3.options[opt3.selectedIndex].value;
+
+  if (daytime == "PM") {
+    hour += 12;
+  }
+
+  console.log(hour);
+  console.log(minute);
+  console.log(daytime);
+  
   let now = new Date();
-  // allow 14 minutes to fall sleep!
-  now.setMinutes(now.getMinutes() + 14);
+  now.setMinutes(minute);
+  now.setHours(hour);
 
   // calculate sleep cycles!
   for (let i = 1; i <= 6; i++) {
